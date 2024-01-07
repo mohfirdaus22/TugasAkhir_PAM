3ac3869785d68a74de0d46f83078fe856c0b6241
ZaqyMunif <zaqymunif17@gmail.com>
2024-01-07 17:05:30 +0700

    membuat header "MENU" pada HalamanUtama

--- app/src/main/java/com/example/tugasakhir_pam/HalamanHome.kt
+++ app/src/main/java/com/example/tugasakhir_pam/HalamanHome.kt
@@ -1,2 +1,32 @@
 package com.example.tugasakhir_pam
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.google.firebase.inappmessaging.model.Button
+
+@Composable
+fun HalamanUtama(
+    
+) {
+    Row (
+        modifier = Modifier.fillMaxWidth().padding(16.dp),
+        horizontalArrangement = Arrangement.Center
+    ){
+        Text(text = "MENU", style = MaterialTheme.typography.titleLarge)
+    }
+}
+
+
