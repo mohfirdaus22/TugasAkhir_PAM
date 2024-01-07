84e7afc9db08ad84443d009a31191855269f9155
mohfirdaus22 <thepashter6@gmail.com>
2024-01-07 15:40:43 +0700

    Add card for HalamanHome

--- app/src/main/java/com/example/tugasakhir_pam/HalamanStart.kt
+++ app/src/main/java/com/example/tugasakhir_pam/HalamanStart.kt
@@ -1,10 +1,19 @@
 package com.example.tugasakhir_pam
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HalamanHome(
@@ -15,6 +24,17 @@ fun HalamanHome(
         modifier = Modifier,
         verticalArrangement = Arrangement.SpaceBetween
     ) {
+        OutlinedCard(
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.surface,
+            ),
+            border = BorderStroke(1.dp, Color.Black), modifier = Modifier
+                .fillMaxWidth(0.95f)
+                .padding(vertical = 50.dp)
+                .align(Alignment.CenterHorizontally)
+        ) {
+
+        }
 
     }
 }
