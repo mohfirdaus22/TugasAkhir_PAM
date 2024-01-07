363c02ed1420d5f710d35bfd96cb1e5bcb621a48
mohfirdaus22 <thepashter6@gmail.com>
2024-01-08 00:01:33 +0700

    Add PenghuniRepository

---
+++ app/src/main/java/com/example/tugasakhir_pam/data/asrama_repository.kt
@@ -0,0 +1,18 @@
+package com.example.tugasakhir_pam.data
+
+import com.example.tugasakhir_pam.model.Penghuni
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.tasks.await
+
+interface PenghuniRepository {
+    fun getAll(): Flow<List<Penghuni>>
+    suspend fun save(asrama: Penghuni): String
+    suspend fun update(asrama: Penghuni)
+    suspend fun delete(penghuniId: String)
+    fun getKontakById(penghuniId: String): Flow<Penghuni>
+}
+
