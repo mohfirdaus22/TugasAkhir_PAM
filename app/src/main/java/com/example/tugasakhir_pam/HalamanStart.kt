28cac72ea02ca44d54089d336343f2a6b056d984
mohfirdaus22 <thepashter6@gmail.com>
2024-01-07 15:38:52 +0700

    Add HalamanHome

---
+++ app/src/main/java/com/example/tugasakhir_pam/HalamanStart.kt
@@ -0,0 +1,20 @@
+package com.example.tugasakhir_pam
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+
+@Composable
+fun HalamanHome(
+    onNextButtonClicked: () -> Unit
+){
+    val image = painterResource(id = R.drawable.logoasramaku)
+    Column(
+        modifier = Modifier,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+
+    }
+}
