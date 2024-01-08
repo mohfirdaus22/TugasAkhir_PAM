6298cea2fa6cba9a52b6ef64204eddebb8cda6b1
mohfirdaus22 <thepashter6@gmail.com>
2024-01-08 17:41:37 +0700

    Add fun FormInputPenghuni

--- app/src/main/java/com/example/tugasakhir_pam/ui/Penghuni/Add/AddPenghuni.kt
+++ app/src/main/java/com/example/tugasakhir_pam/ui/Penghuni/Add/AddPenghuni.kt
@@ -1,5 +1,7 @@
 package com.example.tugasakhir_pam.ui.Penghuni.Add
 
+import androidx.compose.ui.Modifier
+import com.example.tugasakhir_pam.AddEventPenghuni
 import com.example.tugasakhir_pam.navigation.DestinasiNavigasi
 
 object DestinasiEntryPenghuni : DestinasiNavigasi{
@@ -7,3 +9,11 @@ object DestinasiEntryPenghuni : DestinasiNavigasi{
     override val titleRes = "Entry_Penghuni"
 }
 
+fun FormInputPenghuni(
+    addEventPenghuni: AddEventPenghuni,
+    modifier: Modifier = Modifier,
+    onValueChangePenghuni: (AddEventPenghuni) -> Unit = {},
+    enabled: Boolean = true
+){
+
+}
