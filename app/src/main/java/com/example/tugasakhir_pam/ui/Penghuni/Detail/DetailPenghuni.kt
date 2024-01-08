394c9e6d2fe808cd0b40255a8fb01af0dc97c47f
mohfirdaus22 <thepashter6@gmail.com>
2024-01-08 20:18:16 +0700

    Add card and column for ItemDetailsPenghuni

--- app/src/main/java/com/example/tugasakhir_pam/ui/Penghuni/Detail/DetailPenghuni.kt
+++ app/src/main/java/com/example/tugasakhir_pam/ui/Penghuni/Detail/DetailPenghuni.kt
@@ -1,11 +1,19 @@
 package com.example.tugasakhir_pam.ui.Penghuni.Detail
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import com.example.tugasakhir_pam.model.Penghuni
 
 @Composable
@@ -13,7 +21,22 @@ fun ItemDetailsPenghuni(
     penghuni: Penghuni,
     modifier: Modifier = Modifier
 ){
+    Card(
+        modifier = modifier,
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
 
+        }
+    }
 }
 
 @Composable
