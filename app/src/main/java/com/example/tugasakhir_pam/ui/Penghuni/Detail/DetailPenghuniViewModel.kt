88eb528ecefb20c45e2a4a0ae00755bfbe403f4f
mohfirdaus22 <thepashter6@gmail.com>
2024-01-08 20:58:09 +0700

    Add DetailPenghuniViewModel

--- app/src/main/java/com/example/tugasakhir_pam/ui/Penghuni/Detail/DetailPenghuniViewModel.kt
+++ app/src/main/java/com/example/tugasakhir_pam/ui/Penghuni/Detail/DetailPenghuniViewModel.kt
@@ -1,4 +1,37 @@
 package com.example.tugasakhir_pam.ui.Penghuni.Detail
 
-class DetailPenghuniViewModel {
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.tugasakhir_pam.DetailUIStatePenghuni
+import com.example.tugasakhir_pam.data.PenghuniRepository
+import com.example.tugasakhir_pam.toDetailPenghuni
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+class DetailPenghuniViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repository: PenghuniRepository
+): ViewModel(){
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+
+    val penghuniId: String = checkNotNull(savedStateHandle[DetailDestinationPenghuni.penghuniId])
+    val uiStatePenghuni: StateFlow<DetailUIStatePenghuni> =
+        repository.getPenghuniById(penghuniId)
+            .filterNotNull()
+            .map {
+                DetailUIStatePenghuni(addEventPenghuni = it.toDetailPenghuni())
+            }.stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+                initialValue = DetailUIStatePenghuni()
+            )
+    suspend fun deletePenghuni(){
+        repository.delete(penghuniId)
+    }
 }
