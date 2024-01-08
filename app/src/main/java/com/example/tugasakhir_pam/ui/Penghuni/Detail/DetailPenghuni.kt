461ea7edf41de3b5e945a9f83fd7536b382b8090
mohfirdaus22 <thepashter6@gmail.com>
2024-01-08 20:00:42 +0700

    Add DetailPenghuni.kt dan private fun ItemDetailsRowPenghuni

---
+++ app/src/main/java/com/example/tugasakhir_pam/ui/Penghuni/Detail/DetailPenghuni.kt
@@ -0,0 +1,13 @@
+package com.example.tugasakhir_pam.ui.Penghuni.Detail
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+private fun ItemDetailsRowPenghuni(
+    labelResIDPenghuni: String,
+    itemDetailPenghuni: String,
+    modifier: Modifier = Modifier
+){
+
+}
