3cd3d29d72de6d3afcb6d7c68e0da3a33da328c2
mohfirdaus22 <thepashter6@gmail.com>
2024-01-08 17:38:52 +0700

    Add objectDestinasiEntryPenghuni

---
+++ app/src/main/java/com/example/tugasakhir_pam/ui/Penghuni/Add/AddPenghuni.kt
@@ -0,0 +1,9 @@
+package com.example.tugasakhir_pam.ui.Penghuni.Add
+
+import com.example.tugasakhir_pam.navigation.DestinasiNavigasi
+
+object DestinasiEntryPenghuni : DestinasiNavigasi{
+    override val route = "item_entry"
+    override val titleRes = "Entry_Penghuni"
+}
+
