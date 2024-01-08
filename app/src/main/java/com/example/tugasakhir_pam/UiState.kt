3bcc1ec368867afed1ccab94c11030d0085a6451
mohfirdaus22 <thepashter6@gmail.com>
2024-01-08 17:06:32 +0700

    Add dataclass AddEventPenghuni

--- app/src/main/java/com/example/tugasakhir_pam/UiState.kt
+++ app/src/main/java/com/example/tugasakhir_pam/UiState.kt
@@ -6,3 +6,11 @@ data class HomeUIStatePenghuni(
     val listAnggota: List<Penghuni> = listOf(),
     val dataLength: Int = 0
 )
+
+data class  AddEventPenghuni(
+    val id: String = "",
+    val name: String = "",
+    val alamat: String = "",
+    val nohp: String = "",
+    val email: String = ""
+)
