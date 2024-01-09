20c801c0a7f66f38a6149fad8f4d30f419ad8062
mohfirdaus22 <thepashter6@gmail.com>
2024-01-09 17:06:44 +0700

    Complete AnggotaScreen

--- app/src/main/java/com/example/tugasakhir_pam/ui/Penghuni/HomePenghuni/HomePenghuni.kt
+++ app/src/main/java/com/example/tugasakhir_pam/ui/Penghuni/HomePenghuni/HomePenghuni.kt
@@ -1,9 +1,20 @@
 package com.example.tugasakhir_pam.ui.Penghuni.HomePenghuni
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
+import com.example.tugasakhir_pam.PenghuniTopAppBar
 import com.example.tugasakhir_pam.PenyediaViewModel
 import com.example.tugasakhir_pam.navigation.DestinasiNavigasi
 
@@ -20,4 +31,36 @@ fun PenghuniScreen(
     viewModel: HalamanPenghuniViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ) {
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            PenghuniTopAppBar(
+                title = "Penghuni",
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick ={ navigateToItemEntryPenghuni() },
+
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = ""
+                )
+            }
+        }
+    ) { innerPadding ->
+        val uiStatePenghuni by viewModel.homeUIStatePenghuni.collectAsState()
+        BodyHomePenghuni(
+            itemAnggota = uiStatePenghuni.listPenghuni,
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            onAnggotaClick = onDetailClickPenghuni
+        )
+    }
 }
