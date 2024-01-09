12f13ceee46dc69675fd359205daccfe159e4e08
ZaqyMunif <zaqymunif17@gmail.com>
2024-01-10 04:04:26 +0700

    menambahkan AddKamarViewModelmar

--- app/src/main/java/com/example/tugasakhir_pam/ui/Kamar/AddKamar/AddKamarViewModel.kt
+++ app/src/main/java/com/example/tugasakhir_pam/ui/Kamar/AddKamar/AddKamarViewModel.kt
@@ -1,4 +1,22 @@
 package com.example.tugasakhir_pam.ui.Kamar.AddKamar
 
-class AddKamarViewModel {
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.tugasakhir_pam.AddEventKamar
+import com.example.tugasakhir_pam.AddUIStateKamar
+import com.example.tugasakhir_pam.data.KamarRepository
+import com.example.tugasakhir_pam.toKamar
+
+class AddKamarViewModel (private val kamarRepository: KamarRepository): ViewModel(){
+    var addUIStateKamar by mutableStateOf(AddUIStateKamar())
+        private set
+
+    fun updateAddUIStateKamar(addEventKamar: AddEventKamar) {
+        addUIStateKamar = AddUIStateKamar(addEventKamar = addEventKamar)
+    }
+    suspend fun addKamar() {
+        kamarRepository.save(addUIStateKamar.addEventKamar.toKamar())
+    }
 }
