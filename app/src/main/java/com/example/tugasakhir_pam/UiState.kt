0438651910de10040719e3a12114b2fab6583a77
ZaqyMunif <zaqymunif17@gmail.com>
2024-01-10 04:03:42 +0700

    menambahkan uiState untuk kamar

--- app/src/main/java/com/example/tugasakhir_pam/UiState.kt
+++ app/src/main/java/com/example/tugasakhir_pam/UiState.kt
@@ -1,9 +1,15 @@
 package com.example.tugasakhir_pam
 
+import com.example.tugasakhir_pam.model.Kamar
 import com.example.tugasakhir_pam.model.Penghuni
 
 data class HomeUIStatePenghuni(
     val listPenghuni: List<Penghuni> = listOf(),
+    val dataLength: Int = 0
+)
+
+data class HomeUIStateKamar(
+    val listKamar: List<Kamar> = listOf(),
     val dataLength: Int = 0
 )
 
@@ -15,12 +21,26 @@ data class  AddEventPenghuni(
     val email: String = ""
 )
 
+data class  AddEventKamar(
+    val nokamar: String = "",
+    val tipe: String = "",
+    val kapasitas: String = "",
+    val harga: String = ""
+)
+
 fun Penghuni.toDetailPenghuni(): AddEventPenghuni = AddEventPenghuni(
     id = id,
     name = name,
     alamat = alamat,
     nohp = nohp,
     email = email
+)
+
+fun Kamar.toDetailKamar(): AddEventKamar = AddEventKamar(
+    nokamar = nokamar,
+    tipe = tipe,
+    kapasitas = kapasitas,
+    harga = harga
 )
 
 fun AddEventPenghuni.toPenghuni() = Penghuni(
@@ -31,14 +51,33 @@ fun AddEventPenghuni.toPenghuni() = Penghuni(
     email = email
 )
 
+fun AddEventKamar.toKamar() = Kamar(
+    nokamar = nokamar,
+    tipe = tipe,
+    kapasitas = kapasitas,
+    harga = harga
+)
+
 data class AddUIStatePenghuni(
     val addEventPenghuni: AddEventPenghuni = AddEventPenghuni(),
+)
+
+data class AddUIStateKamar(
+    val addEventKamar: AddEventKamar = AddEventKamar()
 )
 
 fun Penghuni.toUIStatePenghuni():AddUIStatePenghuni = AddUIStatePenghuni(
     addEventPenghuni = this.toDetailPenghuni()
 )
 
+fun Kamar.toUIStateKamar():AddUIStateKamar = AddUIStateKamar(
+    addEventKamar = this.toDetailKamar()
+)
+
 data class DetailUIStatePenghuni(
     val addEventPenghuni: AddEventPenghuni = AddEventPenghuni()
+)
+
+data class DetailUIStateKamar(
+    val addEventKamar: AddEventKamar = AddEventKamar()
 )
