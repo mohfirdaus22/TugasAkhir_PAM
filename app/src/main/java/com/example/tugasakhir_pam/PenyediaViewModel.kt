191d4c7e5777df8cbd85c0f03f146a9b453bafc0
mohfirdaus22 <thepashter6@gmail.com>
2024-01-09 16:53:19 +0700

    Add initializer

--- app/src/main/java/com/example/tugasakhir_pam/PenyediaViewModel.kt
+++ app/src/main/java/com/example/tugasakhir_pam/PenyediaViewModel.kt
@@ -1,8 +1,12 @@
 package com.example.tugasakhir_pam
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.tugasakhir_pam.ui.Penghuni.Add.AddPenghuniViewModel
+import com.example.tugasakhir_pam.ui.Penghuni.Detail.DetailPenghuniViewModel
 
 fun CreationExtras.aplikasiAsrama(): AsramaApplication =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AsramaApplication)
@@ -10,5 +14,16 @@ fun CreationExtras.aplikasiAsrama(): AsramaApplication =
 object PenyediaViewModel {
     val Factory = viewModelFactory {
 
+
+        initializer {
+            AddPenghuniViewModel(aplikasiAsrama().container.penghuniRepository)
+        }
+
+        initializer {
+            DetailPenghuniViewModel(
+                createSavedStateHandle(),
+                aplikasiAsrama().container.penghuniRepository
+            )
+        }
     }
 }
