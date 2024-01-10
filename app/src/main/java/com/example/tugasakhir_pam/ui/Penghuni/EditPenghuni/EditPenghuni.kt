f7dafe76b4c4010a28ee014be5e938de0db77b12
mohfirdaus22 <thepashter6@gmail.com>
2024-01-11 00:46:21 +0700

    Add EditScreenPenghuni

--- app/src/main/java/com/example/tugasakhir_pam/ui/Penghuni/EditPenghuni/EditPenghuni.kt
+++ app/src/main/java/com/example/tugasakhir_pam/ui/Penghuni/EditPenghuni/EditPenghuni.kt
@@ -1,6 +1,9 @@
 package com.example.tugasakhir_pam.ui.Penghuni.EditPenghuni
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.example.tugasakhir_pam.navigation.DestinasiNavigasi
+import com.example.tugasakhir_pam.ui.PenyediaViewModel
 
 object EditDestinationPenghuni : DestinasiNavigasi {
     override val route = "item edit penghuni"
@@ -9,3 +12,12 @@ object EditDestinationPenghuni : DestinasiNavigasi {
     val routeWithArgs = "${EditDestinationPenghuni.route}/{${penghuniId}"
 }
 
+@Composable
+fun EditScreenPenghuni(
+    navigateBackPenghuni: () -> Unit,
+    onNavigateUpPenghuni: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditPenghuniViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+
+}
