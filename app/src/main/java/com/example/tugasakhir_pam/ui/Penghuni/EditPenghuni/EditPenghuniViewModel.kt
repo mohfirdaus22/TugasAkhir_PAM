ce1c3c8fe5688a47dac7af347e5038c1bde21430
mohfirdaus22 <thepashter6@gmail.com>
2024-01-11 00:48:18 +0700

    Add EditPenghuniViewModel

---
+++ app/src/main/java/com/example/tugasakhir_pam/ui/Penghuni/EditPenghuni/EditPenghuniViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.tugasakhir_pam.ui.Penghuni.EditPenghuni
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.tugasakhir_pam.data.PenghuniRepository
+
+class EditPenghuniViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repository: PenghuniRepository
+    ) : ViewModel(){
+}
