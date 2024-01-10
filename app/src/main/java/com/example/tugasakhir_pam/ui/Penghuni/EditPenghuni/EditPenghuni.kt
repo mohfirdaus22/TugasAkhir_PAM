ced2b8bf827bc0f14c8b78aaa84edde45279ca26
mohfirdaus22 <thepashter6@gmail.com>
2024-01-11 00:58:41 +0700

    Add UpdateUIStatePenghuni

--- app/src/main/java/com/example/tugasakhir_pam/ui/Penghuni/EditPenghuni/EditPenghuni.kt
+++ app/src/main/java/com/example/tugasakhir_pam/ui/Penghuni/EditPenghuni/EditPenghuni.kt
@@ -1,9 +1,18 @@
 package com.example.tugasakhir_pam.ui.Penghuni.EditPenghuni
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.tugasakhir_pam.navigation.DestinasiNavigasi
+import com.example.tugasakhir_pam.ui.Penghuni.AddPenghuni.EntryBodyPenghuni
 import com.example.tugasakhir_pam.ui.PenyediaViewModel
+import kotlinx.coroutines.launch
 
 object EditDestinationPenghuni : DestinasiNavigasi {
     override val route = "item edit penghuni"
@@ -19,5 +28,22 @@ fun EditScreenPenghuni(
     modifier: Modifier = Modifier,
     viewModel: EditPenghuniViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ) {
+    val coroutineScope = rememberCoroutineScope()
 
+    Scaffold() { innerPadding ->
+        EntryBodyPenghuni(
+            addUIStatePenghuni = viewModel.penghuniUIState,
+            onPenghuniValueChange = viewModel::updateUIStatePenghuni,
+            onSaveClickPenghuni = {
+                coroutineScope.launch {
+                    viewModel.updatePenghuni()
+                    navigateBackPenghuni()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
 }
--- app/src/main/java/com/example/tugasakhir_pam/ui/Penghuni/EditPenghuni/EditPenghuniViewModel.kt
+++ app/src/main/java/com/example/tugasakhir_pam/ui/Penghuni/EditPenghuni/EditPenghuniViewModel.kt
@@ -1,11 +1,41 @@
 package com.example.tugasakhir_pam.ui.Penghuni.EditPenghuni
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.tugasakhir_pam.data.PenghuniRepository
+import com.example.tugasakhir_pam.ui.AddEventPenghuni
+import com.example.tugasakhir_pam.ui.AddUIStatePenghuni
+import com.example.tugasakhir_pam.ui.toPenghuni
+import com.example.tugasakhir_pam.ui.toUIStatePenghuni
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 class EditPenghuniViewModel (
     savedStateHandle: SavedStateHandle,
     private val repository: PenghuniRepository
     ) : ViewModel(){
+    var penghuniUIState by mutableStateOf(AddUIStatePenghuni())
+        private set
+    private val penghuniId: String = checkNotNull(savedStateHandle[EditDestinationPenghuni.penghuniId])
+    init {
+        viewModelScope.launch {
+            penghuniUIState =
+                repository.getPenghuniById(penghuniId)
+                    .filterNotNull()
+                    .first()
+                    .toUIStatePenghuni()
+        }
+    }
+    fun updateUIStatePenghuni(addEventPenghuni: AddEventPenghuni) {
+        penghuniUIState = penghuniUIState.copy(addEventPenghuni = addEventPenghuni)
+    }
+
+    suspend fun updatePenghuni() {
+        repository.update(penghuniUIState.addEventPenghuni.toPenghuni())
+    }
 }
