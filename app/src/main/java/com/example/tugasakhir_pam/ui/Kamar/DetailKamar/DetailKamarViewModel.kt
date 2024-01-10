167a017db3b2ad9538ec2c4d07cfdd64abc988ef
ZaqyMunif <zaqymunif17@gmail.com>
2024-01-10 20:07:33 +0700

    membuat viewmodel untuk detail kamar

--- app/src/main/java/com/example/tugasakhir_pam/ui/Kamar/DetailKamar/DetailKamarViewModel.kt
+++ app/src/main/java/com/example/tugasakhir_pam/ui/Kamar/DetailKamar/DetailKamarViewModel.kt
@@ -1,4 +1,38 @@
 package com.example.tugasakhir_pam.ui.Kamar.DetailKamar
 
-class DetailKamarViewModel {
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.tugasakhir_pam.DetailUIStateKamar
+import com.example.tugasakhir_pam.DetailUIStatePenghuni
+import com.example.tugasakhir_pam.data.KamarRepository
+import com.example.tugasakhir_pam.toDetailKamar
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+class DetailKamarViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repository: KamarRepository
+): ViewModel(){
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+
+    val kamarId: String = checkNotNull(savedStateHandle[DetailDestinationKamar.kamarId])
+    val uiStateKamar: StateFlow<DetailUIStateKamar> =
+        repository.getKamarById(kamarId)
+            .filterNotNull()
+            .map {
+                DetailUIStateKamar(addEventKamar = it.toDetailKamar())
+            }.stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+                initialValue = DetailUIStateKamar()
+            )
+    suspend fun deleteKamar(){
+        repository.delete(kamarId)
+    }
 }
